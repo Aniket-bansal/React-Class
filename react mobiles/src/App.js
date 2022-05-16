aacee792f8e3aceab379aa0ac38976fbf736aef2
Aniket bansal <77097101+Aniket-bansal@users.noreply.github.com>
2022-05-16 18:03:29 +0530

    react mobile

---
+++ react mobiles/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+function App(){
+  return (
+    <>
+    <ul>Mobile Operating System:
+      <li>Android</li>
+      <li>Blackberry</li>
+      <li>iPhone</li>
+      <li>Windows Phone</li>
+    </ul>
+    <br/>
+    <ul>Mobile Manufacturers:
+      <li>Samsung</li>
+      <li>HTC</li>
+      <li>Micromax</li>
+      <li>Apple</li>
+    </ul>
+    </>
+  );
+}
+
+
+export default App;
