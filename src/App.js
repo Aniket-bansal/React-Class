a6e7eab106ce843042ffe0a6a1ad0ed3be136e67
Aniket Bansal <akbaisla29041998@gmail.com>
2022-05-16 17:58:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ul>Mobile Operating System:
+      <li>Android</li>
+      <li>Blackberry</li>
+      <li>iPhone</li>
+      <li>Windows Phone</li>
+    </ul>
+    <br/>
+    <ul>Mobile Manufacturers:
+      <li>Samsung</li>
+      <li>HTC</li>
+      <li>Micromax</li>
+      <li>Apple</li>
+    </ul>
+    </>
   );
 }
+
 
 export default App;
