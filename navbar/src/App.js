f58aeffea0ee18bb690b477eb804b449a56347aa
Aniket bansal <77097101+Aniket-bansal@users.noreply.github.com>
2022-05-17 11:41:19 +0530

    Add files via upload

---
+++ navbar/src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+function App() {
+  return (
+    <>
+    <div id="navbar">
+    <h1>LOGOBAKERY</h1>
+    <div id="links">
+      <h1>Services</h1>
+      <h1>Projects</h1>
+      <h1>About</h1>
+    </div>
+    <button id="btn">Contact</button>
+    </div>
+    </>
+  );
+}
+
+export default App;
